1bf60f593329a2e45394e1966530c56fc302aa7f
Connor Wyatt <connorwyatt1@gmail.com>
2024-12-01 22:50:14 +0000

    Day 1 part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -3,9 +3,10 @@ use std::{fs, iter::zip};
 fn main() {
     let input = fs::read_to_string("./day-1/src/input.txt").expect("input.txt could not be found");
 
-    println!("Result: {:?}", total_distance_between_lists(input));
+    println!("Result: {:?}", similarity_score(input));
 }
 
+#[allow(dead_code)]
 fn total_distance_between_lists(input: String) -> usize {
     let (mut left, mut right): (Vec<_>, Vec<_>) =
         input.lines().map(|line| parse_input_line(line)).unzip();
@@ -15,6 +16,20 @@ fn total_distance_between_lists(input: String) -> usize {
 
     zip(left, right)
         .map(|(first, second)| first.abs_diff(second))
+        .sum()
+}
+
+fn similarity_score(input: String) -> usize {
+    let (left, right): (Vec<_>, Vec<_>) = input.lines().map(|line| parse_input_line(line)).unzip();
+
+    left.iter()
+        .map(|left_number| {
+            right
+                .iter()
+                .filter(|&right_number| right_number == left_number)
+                .count()
+                * left_number
+        })
         .sum()
 }
 
@@ -41,5 +56,10 @@ mod test {
     #[test]
     fn total_distance_between_lists_works() {
         assert_eq!(total_distance_between_lists(INPUT.to_string()), 11)
+    }
+
+    #[test]
+    fn similarity_score_works() {
+        assert_eq!(similarity_score(INPUT.to_string()), 31)
     }
 }
