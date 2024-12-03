45a542d3991b7b960d7ba75494568ce5156eb5cc
Connor Wyatt <connorwyatt1@gmail.com>
2024-12-03 00:45:06 +0000

    Day 2 part 2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,43 +1,36 @@
 const INPUT: &str = include_str!("aoc-input/input.txt");
 
 fn main() {
-    println!("Result: {:?}", count_safe_reports(INPUT));
+    println!("Result: {:?}", count_safe_reports_with_tolerance(INPUT));
 }
 
+#[allow(dead_code)]
 fn count_safe_reports(input: &str) -> usize {
     input
         .lines()
         .map(parse_line)
-        .filter(|numbers| {
-            let mut windowed_numbers = numbers.windows(2);
+        .filter(|report| is_valid_report(report.as_slice()))
+        .count()
+}
 
-            let [first, second] = windowed_numbers.next().unwrap() else {
-                panic!("windows should be 2 items long");
-            };
-
-            match first.abs_diff(*second) {
-                1..=3 => {}
-                _ => return false,
+fn count_safe_reports_with_tolerance(input: &str) -> usize {
+    input
+        .lines()
+        .map(parse_line)
+        .filter(|report| {
+            if is_valid_report(report.as_slice()) {
+                return true;
             }
 
-            let is_increasing = second > first;
+            for index in 0..report.len() {
+                let modified_report = [&report[..index], &report[index + 1..]].concat();
 
-            while let Some([first, second]) = windowed_numbers.next() {
-                if first == second {
-                    return false;
-                }
-
-                if (is_increasing && second <= first) || (!is_increasing && second >= first) {
-                    return false;
-                }
-
-                match first.abs_diff(*second) {
-                    1..=3 => {}
-                    _ => return false,
+                if is_valid_report(modified_report.as_slice()) {
+                    return true;
                 }
             }
 
-            true
+            false
         })
         .count()
 }
@@ -46,6 +39,38 @@ fn parse_line(line: &str) -> Vec<usize> {
     line.split_ascii_whitespace()
         .map(|number| number.parse::<usize>().unwrap())
         .collect::<Vec<_>>()
+}
+
+fn is_valid_report(report: &[usize]) -> bool {
+    let mut windowed_report = report.windows(2);
+
+    let [first, second] = windowed_report.next().unwrap() else {
+        panic!("windows should be 2 items long");
+    };
+
+    match first.abs_diff(*second) {
+        1..=3 => {}
+        _ => return false,
+    }
+
+    let is_increasing = second > first;
+
+    while let Some([first, second]) = windowed_report.next() {
+        if first == second {
+            return false;
+        }
+
+        if (is_increasing && second <= first) || (!is_increasing && second >= first) {
+            return false;
+        }
+
+        match first.abs_diff(*second) {
+            1..=3 => {}
+            _ => return false,
+        }
+    }
+
+    true
 }
 
 #[cfg(test)]
@@ -57,5 +82,10 @@ mod test {
     #[test]
     fn count_safe_reports_works() {
         assert_eq!(count_safe_reports(EXAMPLE_INPUT), 2)
+    }
+
+    #[test]
+    fn count_safe_reports_with_tolerance_works() {
+        assert_eq!(count_safe_reports_with_tolerance(EXAMPLE_INPUT), 4)
     }
 }
