4e559e3fb4c0bb16b8e925f910fb822e7eedd8a2
Connor Wyatt <connorwyatt1@gmail.com>
2024-12-19 00:06:24 +0000

    Day 10 part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -7,9 +7,10 @@ use std::{
 const INPUT: &str = include_str!("aoc-input/input.txt");
 
 fn main() {
-    println!("Result: {:?}", sum_of_trailhead_scores(INPUT));
+    println!("Result: {:?}", sum_of_trailhead_ratings(INPUT));
 }
 
+#[allow(dead_code)]
 fn sum_of_trailhead_scores(input: &str) -> u16 {
     let topographical_map = TopographicalMap::from_str(input).unwrap();
 
@@ -17,6 +18,18 @@ fn sum_of_trailhead_scores(input: &str) -> u16 {
         .trailhead_positions()
         .iter()
         .map(|trailhead_position| calculate_trailhead_score(trailhead_position, &topographical_map))
+        .sum()
+}
+
+fn sum_of_trailhead_ratings(input: &str) -> u16 {
+    let topographical_map = TopographicalMap::from_str(input).unwrap();
+
+    topographical_map
+        .trailhead_positions()
+        .iter()
+        .map(|trailhead_position| {
+            calculate_trailhead_rating(trailhead_position, &topographical_map)
+        })
         .sum()
 }
 
@@ -52,6 +65,40 @@ fn calculate_trailhead_score(
     }
 
     nine_height_positions_reachable.len().try_into().unwrap()
+}
+
+fn calculate_trailhead_rating(
+    trailhead_position: &Position,
+    topographical_map: &TopographicalMap,
+) -> u16 {
+    let mut stack = VecDeque::from([(trailhead_position.clone(), 0)]);
+    let mut rating = 0u16;
+
+    while !stack.is_empty() {
+        let (position, height) = stack.pop_back().unwrap();
+
+        for direction in Direction::all() {
+            let Some(translated_position) = position.translate(&direction) else {
+                continue;
+            };
+
+            let Some(translated_position_height) = topographical_map.get(&translated_position)
+            else {
+                continue;
+            };
+
+            if translated_position_height == height + 1 {
+                if translated_position_height == 9 {
+                    rating += 1;
+                    continue;
+                }
+
+                stack.push_back((translated_position, translated_position_height));
+            }
+        }
+    }
+
+    rating
 }
 
 #[derive(Debug)]
@@ -170,6 +217,11 @@ mod test {
     }
 
     #[test]
+    fn sum_of_trailhead_ratings_works() {
+        assert_eq!(sum_of_trailhead_ratings(EXAMPLE_INPUT), 81);
+    }
+
+    #[test]
     fn calculate_trailhead_score_works() {
         let topographical_map = TopographicalMap::from_str(EXAMPLE_INPUT).unwrap();
 
@@ -207,6 +259,48 @@ mod test {
         );
         assert_eq!(
             calculate_trailhead_score(&Position::new(1, 7), &topographical_map),
+            5
+        );
+    }
+
+    #[test]
+    fn calculate_trailhead_rating_works() {
+        let topographical_map = TopographicalMap::from_str(EXAMPLE_INPUT).unwrap();
+
+        assert_eq!(
+            calculate_trailhead_rating(&Position::new(2, 0), &topographical_map),
+            20
+        );
+        assert_eq!(
+            calculate_trailhead_rating(&Position::new(4, 0), &topographical_map),
+            24
+        );
+        assert_eq!(
+            calculate_trailhead_rating(&Position::new(4, 2), &topographical_map),
+            10
+        );
+        assert_eq!(
+            calculate_trailhead_rating(&Position::new(6, 4), &topographical_map),
+            4
+        );
+        assert_eq!(
+            calculate_trailhead_rating(&Position::new(2, 5), &topographical_map),
+            1
+        );
+        assert_eq!(
+            calculate_trailhead_rating(&Position::new(5, 5), &topographical_map),
+            4
+        );
+        assert_eq!(
+            calculate_trailhead_rating(&Position::new(0, 6), &topographical_map),
+            5
+        );
+        assert_eq!(
+            calculate_trailhead_rating(&Position::new(6, 6), &topographical_map),
+            8
+        );
+        assert_eq!(
+            calculate_trailhead_rating(&Position::new(1, 7), &topographical_map),
             5
         );
     }
