a1a5a2269a68372c881d4cb48a6668e0ec6d9ed9
Connor Wyatt <connorwyatt1@gmail.com>
2024-12-30 14:08:59 +0000

    Day 16 part 1 - faster Dijkstra implementation

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,6 +1,7 @@
-use std::{collections::HashSet, f32::consts::PI, rc::Rc};
+use std::collections::HashSet;
 
 use glam::{IVec2, UVec2};
+use pathfinding::prelude::*;
 
 const INPUT: &str = include_str!("aoc-input/input.txt");
 
@@ -11,145 +12,27 @@ fn main() {
 fn calculate_lowest_score(input: &str) -> u32 {
     let maze = parse_input(input);
 
-    let potential_directions = [IVec2::X, IVec2::NEG_X, IVec2::Y, IVec2::NEG_Y];
+    let result = dijkstra(
+        &(maze.start, IVec2::X),
+        |(position, direction)| {
+            let mut successors = vec![
+                ((*position, direction.perp()), 1000),
+                ((*position, -direction.perp()), 1000),
+            ];
+            let next_position = (position.as_ivec2() + direction).as_uvec2();
 
-    let mut lowest_score = None;
-
-    let mut paths_to_check = Vec::from([Rc::new(PathStep {
-        position: maze.start,
-        previous: None,
-        score: 0,
-        priority: calculate_priority(&maze.start, &maze.end, &IVec2::X, false),
-    })]);
-
-    while let Some(path_to_check) = paths_to_check.pop() {
-        if let Some(ls) = lowest_score {
-            if ls < path_to_check.score {
-                continue;
-            }
-        }
-
-        if maze.is_end(&path_to_check.position) {
-            if path_to_check.score < lowest_score.unwrap_or(u32::MAX) {
-                lowest_score = Some(path_to_check.score);
-            }
-            continue;
-        }
-
-        if maze.is_wall(&path_to_check.position) {
-            continue;
-        }
-
-        {
-            let mut current = path_to_check.clone();
-            let mut already_visited = false;
-            while let Some(previous) = &current.previous {
-                if previous.position == path_to_check.position {
-                    already_visited = true;
-                    break;
-                }
-
-                current = previous.clone();
+            if !maze.is_wall(&next_position) {
+                successors.push(((next_position, *direction), 1));
             }
 
-            if already_visited {
-                continue;
-            }
-        }
+            successors
+        },
+        |(position, _direction)| maze.is_end(position),
+    );
 
-        for direction in potential_directions {
-            let position = path_to_check.position.saturating_add_signed(direction);
+    let (_, score) = result.unwrap();
 
-            let Some(previous) = &path_to_check.previous else {
-                let mut score = 1;
-
-                if direction != IVec2::X {
-                    score += 1000;
-                }
-
-                add_path_to_check(
-                    &mut paths_to_check,
-                    Rc::new(PathStep {
-                        position,
-                        previous: Some(path_to_check.clone()),
-                        score,
-                        priority: calculate_priority(&position, &maze.end, &direction, false),
-                    }),
-                );
-                continue;
-            };
-
-            let previous_direction =
-                path_to_check.position.as_ivec2() - previous.position.as_ivec2();
-
-            let mut score = path_to_check.score + 1;
-
-            if direction != previous_direction {
-                score += 1000;
-            }
-
-            add_path_to_check(
-                &mut paths_to_check,
-                Rc::new(PathStep {
-                    position,
-                    previous: Some(path_to_check.clone()),
-                    score,
-                    priority: calculate_priority(
-                        &position,
-                        &maze.end,
-                        &direction,
-                        direction == previous_direction,
-                    ),
-                }),
-            );
-        }
-    }
-
-    lowest_score.unwrap()
-}
-
-fn calculate_priority(
-    position: &UVec2,
-    end: &UVec2,
-    direction: &IVec2,
-    is_same_direction: bool,
-) -> i32 {
-    let distance = (end.as_vec2() - position.as_vec2()).length_squared();
-    let desired_direction_vec = end.as_vec2() - position.as_vec2();
-    let angle_diff = direction.as_vec2().angle_to(desired_direction_vec);
-    let angle_factor = 0.2 + 0.8 * (PI - angle_diff.abs()) / PI;
-    (-distance * angle_factor * if is_same_direction { 1.0 } else { 0.5 } * 100.0).round() as i32
-}
-
-fn add_path_to_check(paths_to_check: &mut Vec<Rc<PathStep>>, path_to_check: Rc<PathStep>) {
-    paths_to_check.push(path_to_check.clone());
-    paths_to_check.sort_unstable_by_key(|p| p.priority);
-}
-
-#[derive(Debug)]
-struct PathStep {
-    position: UVec2,
-    previous: Option<Rc<PathStep>>,
-    score: u32,
-    priority: i32,
-}
-
-impl PathStep {
-    fn points(&self) -> Vec<UVec2> {
-        let mut vec = vec![self.position];
-
-        {
-            let mut current = self.previous.clone();
-            while let Some(c) = current {
-                vec.push(c.position);
-                current = c.previous.clone();
-            }
-        }
-
-        vec.reverse();
-
-        vec
-    }
+    score
 }
 
 struct Maze {
@@ -159,10 +42,6 @@ struct Maze {
 }
 
 impl Maze {
-    fn is_start(&self, position: &UVec2) -> bool {
-        position == &self.start
-    }
-
     fn is_end(&self, position: &UVec2) -> bool {
         position == &self.end
     }
@@ -210,47 +89,6 @@ fn parse_input(input: &str) -> Maze {
     let end = end.unwrap();
 
     Maze { start, end, walls }
-}
-
-#[allow(dead_code)]
-fn debug_path(maze: &Maze, path: Rc<PathStep>) {
-    let area_size = maze
-        .walls
-        .iter()
-        .fold(UVec2::new(0, 0), |acc, &wall| acc.max(wall))
-        + UVec2::ONE;
-
-    let path_points = path.points();
-
-    for y in 0..area_size.y {
-        for x in 0..area_size.x {
-            let position = UVec2 { x, y };
-
-            if maze.is_wall(&position) {
-                print!("#");
-                continue;
-            }
-            let is_path_point = path_points.contains(&position);
-            if maze.is_end(&position) {
-                if is_path_point {
-                    print!("!");
-                } else {
-                    print!("E");
-                }
-                continue;
-            }
-            if maze.is_start(&position) {
-                print!("S");
-                continue;
-            }
-            if is_path_point {
-                print!("█");
-                continue;
-            }
-            print!(" ");
-        }
-        println!();
-    }
 }
 
 #[cfg(test)]
